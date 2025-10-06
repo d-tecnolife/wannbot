67d7c8f1438adfb4ce818c658754f9b8ccd2cb0e
d-tecnolife <dtecnolife@proton.me>
2025-10-06 05:39:33 +0000

    initial bot creation

---
+++ wannbot.py
@@ -0,0 +1,28 @@
+import os
+
+import discord
+import dotenv
+
+dotenv.load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "Sup You Bum Ass <:canit:1221156920623370250>" in message.content:
+        await message.channel.send("Hello How Are You")
+
+
+client.run(os.getenv("token"))
